250b527c1058b78136dafc1b8354da3044e28668
tecnology8 <ruddycruceta@hotmail.com>
2019-11-15 11:39:07 -0400

    Inspector, bundles y tree-shaking DI

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,13 @@ import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello/hello.component';
 import { UpperCapitalPipe } from './hello/upper-capital.pipe';
-import { LoggerService } from './logger.service';
+
 import { GreetingsService } from './greetings.service';
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
+  imports:[ BrowserModule, FormsModule ],
   declarations: [ AppComponent, HelloComponent, UpperCapitalPipe ],
-  providers:    [LoggerService, GreetingsService ],
+  providers:    [ GreetingsService ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
